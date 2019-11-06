5a7125d4975af6f670b29f90b1c99f18bc86d67b
Dantte169 <Dantte169@abv.bg>
2019-11-06 17:32:42 +0200

    Sorting

---
+++ 01. Working with Abstraction - Lab/Lab/03. StudentSystem/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentSystem
+{
+    public class StartUp
+    {
+        private static void Main()
+        {
+            StudentSystem studentSystem = new StudentSystem();
+
+            while (true)
+            {
+                studentSystem.ParseCommand();
+            }
+        }
+    }
+}
---
+++ 01. Working with Abstraction - Lab/Lab/04. HotelReservation/DiscountType.cs
@@ -0,0 +1,9 @@
+namespace HotelReservation
+{
+    public enum DiscountType
+    {
+        VIP = 20,
+        SecondVisit = 10,
+        None = 0
+    }
+}
